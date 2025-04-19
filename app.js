4f98ea433bc9b1670798e43f3cf91c3b5fbf1de6
Yoalvarado25 <manacima2019@gmail.com>
2025-04-19 20:41:31 +0000

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let oneEuroIs = {
+    "JPY": 156.5, // Japan yen
+    "USD": 1.07, // US dollar
+    "GBP": 0.87, // British pound
+};
+
+function fromDollarToYen(dollars) {
+  return (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+}
+
+ function fromEuroToDollar(euros) {
+   return euros * oneEuroIs["USD"];
+    } 
+
+function fromYenToPound(yen) {
+    return (yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
+    }
+
+    module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
